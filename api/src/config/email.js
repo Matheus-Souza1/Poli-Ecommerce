7e8426648471aea81cf7897cd3701b42f4ecad41
Matheus <matheussouzaslv2@gmail.com>
2020-05-08 17:08:16 -0300

    Arquivos de configuracao

---
+++ api/src/config/email.js
@@ -0,0 +1,8 @@
+module .exports = {
+    host:"smtp.gmail.com",
+    port:465,
+    auth: {
+        user:"matheussouza@gmail.com",
+        pass:"12345"
+    }
+};
---
+++ api/src/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    secret: process.env.NODE_ENV === "production" ? process.env.SECRET : "GDSggdg5454545gds4545145f4ds545f124f5d4s51ds54f5as",
+    api: process.env.NODE_ENV === "production" ? "https://api.loja-teste.amplie.com" : "http://localhost:3000",
+    loja: process.env.NODE_ENV ===  "production" ? "https://loja-teste.amplie.com" : "http://localhost:8000"
+
+};
