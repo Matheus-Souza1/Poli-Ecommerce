856324471c975ec31ba6a5a1d48efb16c41372db
Matheus Souza <54037557+Matheus-Souza1@users.noreply.github.com>
2020-12-10 13:36:23 -0300

    Atualizando Implementação

--- config/pagseguro.js
+++ config/pagseguro.js
@@ -1,8 +1,8 @@
 module.exports = {
     mode: process.env.NODE_ENV === "production" ? "live" : "sandbox",
     sandbox:  process.env.NODE_ENV === "production" ? false:true,
-    sandbox_email:  process.env.NODE_ENV === "production" ? null: "c56800673676688913880@sandbox.pagseguro.com.br",
-    email:"matheussouzaslv2@gmail.com",
-    token: "8BCEABE08FB8415EBD7F9BBB6044349A",
+    sandbox_email:  process.env.NODE_ENV === "production" ? null: "**@sandbox.pagseguro.com.br",
+    email:"**",
+    token: "**",
     notificationURL: "https://api.loja-teste.ampliee.com/v1/api/pagamentos/notificacao"
 };
